961959ad830d9c03c5be404155c86a8b92111558
tanuchoudhary21 <chtaruna90@gmail.com>
2021-05-19 02:47:42 +0530

    Added functionality to unfavourite and changing tabs

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import { data } from '../data';
-import { addMovies } from '../actions';
+import { addMovies, addFavourite, setShowFavourites } from '../actions';
 
 class App extends React.Component {
 
@@ -17,21 +17,43 @@ class App extends React.Component {
     store.dispatch(addMovies(data));
   }
 
+  isMovieFavourite = (movie) => {
+      const { favourites } = this.props.store.getState();
+      const index = favourites.indexOf(movie);
+
+      if(index !== -1){
+        return true; // found the moviee
+      }
+      return false;
+  }
+
+  onChangeTab = (val) => {
+    this.props.store.dispatch(setShowFavourites(val))
+  }
+
   render(){
-    const { list } = this.props.store.getState();
+    const { list , favourites, showFavourites } = this.props.store.getState();
+
+    const displayMovies = showFavourites ? favourites : list;
     return (
       <div className="App">
         <Navbar/>
         <div className = "main">
           <div className = "tabs">
-            <div className = "tab">Movies</div>
-            <div className = "tab">Favourites</div>
+            <div className = {`tab ${showFavourites ? '' : 'active-tabs' }`} onClick = {()=> this.onChangeTab(false)} >Movies</div>
+            <div className = {`tab ${showFavourites ? 'active-tabs' : '' }`} onClick = {()=> this.onChangeTab(true)} >Favourites</div>
           </div>
           <div className = "list">
-            {list.map((movie, index) => (
-              <MovieCard movie = {movie} key = {`movies-${index}`} />
+            {displayMovies.map((movie, index) => (
+              <MovieCard 
+                movie = {movie} 
+                key = {`movies-${index}`} 
+                dispatch = {this.props.store.dispatch}
+                isFavourite = {this.isMovieFavourite(movie)} 
+              />
             ))}
           </div>
+          { displayMovies.length === 0 ? <div className = "no-movies" >No movies to display!! </div> : null }
         </div>
       </div>
     );
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { addFavourite, removeFromFavourites } from '../actions';
 
 class MovieCard extends React.Component{
-    render(){
+
+    handleFavouriteClick = () => {
         const { movie } = this.props;
+        this.props.dispatch(addFavourite(movie))
+    }
+
+    handleUnFavouriteClick = () => {
+        const { movie } = this.props;
+        this.props.dispatch(removeFromFavourites(movie));
+    }
+
+    render(){
+        const { movie , isFavourite} = this.props;
         return(
             <div className ="movie-card">
                 <div className = "left">
@@ -13,7 +25,11 @@ class MovieCard extends React.Component{
                     <div className = "plot"> {movie.Plot} </div>
                     <div className = "footer">
                         <div className = "rating"> {movie.imdbRating} </div>
-                        <button className = "favourite-btn">Favourite</button>
+                        {
+                            isFavourite
+                            ?<button className = "favourite-btn" onClick = { this.handleUnFavouriteClick }>Unfavourite</button>
+                            :<button className = "favourite-btn" onClick = { this.handleFavouriteClick }>Favourite</button>
+                        }
                     </div>
                 </div>
             </div>
